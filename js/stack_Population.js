3faac93900df5ceea980bdfd22c2d1d236e5dd0d
GeethaNehru <geetha8dec@gmail.com>
2018-02-02 12:06:00 +0530

    Update stack_Population.js

--- js/stack_Population.js
+++ js/stack_Population.js
@@ -1,6 +1,6 @@
 var margin={top:40, bottom:200, left:150, right:150},
     width=1500-margin.left-margin.right,
-    height=1000-margin.top-margin.bottom;
+    height=700-margin.top-margin.bottom;
 
   var horizontal=d3.scale.ordinal().rangeRoundBands([0,width],0.10),
     vertical=d3.scale.linear().rangeRound([height,0]);
